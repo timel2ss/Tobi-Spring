959069237ddc4355c2ad6a19e2aaeae3723fd76b
2019054957 <dyddn0823@hanyang.ac.kr>
2021-02-24 00:31:02 +0900

    1.6 싱글톤 레지스트리와 오브젝트 스코프

--- src/main/java/dao/UserDaoTest.java
+++ src/main/java/dao/UserDaoTest.java
@@ -1,12 +1,16 @@
 package dao;
 
 import domain.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao userDao = new DaoFactory().userDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+
+        UserDao userDao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
         user.setId("whiteship");
@@ -22,5 +26,20 @@ public class UserDaoTest {
         System.out.println(user2.getPassword());
 
         System.out.println(user2.getId() + " 조회 성공");
+
+        // 리스트 1-20 직접 생성한 DaoFactory 오브젝트 출력 코드
+        DaoFactory daoFactory = new DaoFactory();
+        UserDao dao1 = daoFactory.userDao();
+        UserDao dao2 = daoFactory.userDao();
+
+        System.out.println(dao1);
+        System.out.println(dao2);
+
+        // 리스트 1-21 스프링 컨텍스트로부터 가져온 오브젝트 출력 코드
+        UserDao dao3 = context.getBean("userDao", UserDao.class);
+        UserDao dao4 = context.getBean("userDao", UserDao.class);
+
+        System.out.println(dao3);
+        System.out.println(dao4);
     }
 }
