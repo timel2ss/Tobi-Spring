d323f2859b41a4f436410e3913c8d2cab3397c7a
2019054957 <dyddn0823@hanyang.ac.kr>
2021-02-23 23:57:35 +0900

    1.1 초난감 DAO

---
+++ src/main/java/dao/UserDao.java
@@ -0,0 +1,63 @@
+package dao;
+
+import domain.User;
+
+import java.sql.*;
+
+public class UserDao {
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("org.mariadb.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mariadb://localhost/tobi_spring", "root", "");
+
+        PreparedStatement psmt = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
+        psmt.setString(1, user.getId());
+        psmt.setString(2, user.getName());
+        psmt.setString(3, user.getPassword());
+
+        psmt.executeUpdate();
+
+        psmt.close();
+        c.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("org.mariadb.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mariadb://localhost/tobi_spring", "root", "");
+
+        PreparedStatement psmt = c.prepareStatement("select * from users where id = ?");
+        psmt.setString(1, id);
+
+        ResultSet rs = psmt.executeQuery();
+        rs.next();
+
+        User user = new User();
+        user.setId(rs.getString(1));
+        user.setName(rs.getString(2));
+        user.setPassword(rs.getString(3));
+
+        rs.close();
+        psmt.close();
+        c.close();
+
+        return user;
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao userDao = new UserDao();
+
+        User user = new User();
+        user.setId("whiteship");
+        user.setName("백기선");
+        user.setPassword("married");
+
+        userDao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = userDao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + " 조회 성공");
+    }
+}
---
+++ src/main/java/domain/User.java
@@ -0,0 +1,11 @@
+package domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter @Getter
+public class User {
+    String id;
+    String name;
+    String password;
+}
